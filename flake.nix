bd2085c13801dec56ce3c54149b99a0268c27126
Paul Meyer <katexochen0@gmail.com>
2025-02-10 10:33:37 +0100

    flake: consume nixos-24.11
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
